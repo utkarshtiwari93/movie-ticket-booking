70d17b1f790756c142600814bf6b30d9529acb8c
utkarshtiwari93 <utkarshtiwari85378@gmail.com>
2026-06-26 12:27:15 +0530

    feat: eureka server configured

---
+++ movie-service/src/test/java/com/utkarsh/movie_service/MovieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utkarsh.movie_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
